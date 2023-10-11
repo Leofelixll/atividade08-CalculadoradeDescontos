e71ab3602e6b874112b242fdf83ae1a13c6d01a9
Leofelixll <100796532+Leofelixll@users.noreply.github.com>
2023-10-11 12:45:18 -0300

    Update atividade08.py

--- atividade08.py
+++ atividade08.py
@@ -1,7 +1,7 @@
 # Informe um valor e veja quanto de desconto você terá em R$, receba a % do desconto que quer receber;
 
 print('Calculadora de descontos')
-valor = float(input('Digite o valor em reais(R$): '))
+valor = float(input('Digite o valor em reais: '))
 porcento = int(input('Digite a porcentagem do desconto(%): '))
 desconto = valor*(porcento/100)
 desconto_total = valor-desconto
